600ded1d6762bc762d2999c57788c871c16714f4
Paul2G <71796809+Paul2G@users.noreply.github.com>
2020-10-16 16:24:50 -0700

    Practica 3
    Copia de la escena 2 con una ligera interacion añadida

---
+++ Assets/SampleScenes/Scripts/Interactable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    bool isInsideZone = false;
+
+    public virtual void Interact ()
+    {
+        
+    }
+
+    void Update ()
+    {
+        if (isInsideZone && Input.GetKeyDown(KeyCode.G))
+        {
+            Interact();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+    	if (!other.CompareTag("Player"))
+    	{
+    		return;
+    	}
+
+        isInsideZone=true;
+    }
+
+        void OnTriggerExit(Collider other)
+    {
+    	if (!other.CompareTag("Player"))
+    	{
+    		return;
+    	}
+
+        isInsideZone=false;
+    }
+}
