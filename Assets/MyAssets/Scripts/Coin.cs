82dd8923f2229f5ac0dc61ff1bc706585030e8d5
Paul2G <71796809+Paul2G@users.noreply.github.com>
2020-11-06 23:52:02 -0800

    Practica 4
    4

---
+++ Assets/MyAssets/Scripts/Coin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : Interactable
+{
+    Rigidbody rb;
+
+    public Vector3 moveDirection;
+
+    public float moveForce = 5f;
+
+    void Awake ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        if(rb!=null)
+        {
+            rb.AddForce(moveDirection * moveForce, ForceMode.Force);
+        }
+    }
+}
---
+++ Assets/MyAssets/Scripts/Pickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class Pickup : Interactable
+{
+    private Inventory inventory;
+    public Item item;
+    void Start()
+    {
+        inventory = FindObjectOfType<Inventory>();
+        if (inventory == null)
+        {
+            Debug.LogWarning("No se encontro el inventario");
+        }
+    }
+    public override void Interact ()
+    {
+        Debug.Log("Levantando item");
+        inventory.Add(item);
+        Destroy(gameObject);    
+    } 
+}
